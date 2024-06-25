2aed69e81eccbb27fd10f0c7864e1d1839105ef5
anteklichy <128155700+anteklichy@users.noreply.github.com>
2024-06-25 13:10:07 +0200

    Last upload
    Final changes (hopefully)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 import http.client
 import json
 
@@ -13,11 +13,17 @@ headers = {
 }
 
 
+def def_res(conn):
+    res = conn.getresponse()
+    if res.status == 404 | res.status == 500:
+        raise Exception()
+    return res.read()
+
+
 def get_api_response(endpoint):
     conn = http.client.HTTPSConnection(API_HOST)
     conn.request("GET", endpoint, headers=headers)
-    res = conn.getresponse()
-    data = res.read()
+    data = def_res(conn)
     conn.close()
     return json.loads(data.decode("utf-8"))
 
@@ -26,8 +32,7 @@ def post_api_response(endpoint, payload):
     conn = http.client.HTTPSConnection(API_HOST)
     headers['Content-Type'] = "application/json"
     conn.request("POST", endpoint, payload, headers)
-    res = conn.getresponse()
-    data = res.read()
+    data = def_res(conn)
     conn.close()
     return json.loads(data.decode("utf-8"))
 
@@ -39,82 +44,111 @@ def index():
 
 @app.route('/cuisines', methods=['GET'])
 def get_cuisines():
-    api_response = get_api_response("/cuisines")
-    cuisines = api_response['cuisines']['data']
-    return render_template('cuisines.html', cuisines=cuisines)
+    try:
+        api_response = get_api_response("/cuisines")
+        cuisines = api_response['cuisines']['data']
+        return render_template('cuisines.html', cuisines=cuisines)
+    except Exception as e:
+        return render_template('error.html', message=str(e))
 
 
 @app.route('/categories', methods=['GET'])
 def get_categories():
-    api_response = get_api_response("/categories")
-    categories = api_response['categories']['data']
-    return render_template('categories.html', categories=categories)
+    try:
+        api_response = get_api_response("/categories")
+        categories = api_response['categories']['data']
+        return render_template('categories.html', categories=categories)
+    except Exception as e:
+        return render_template('error.html', message=str(e))
 
 
 @app.route('/random', methods=['GET'])
 def get_random_recipe():
-    response = get_api_response("/random")
-    recipe = response['recipe']
-    return render_template('base_recipe.html', data=recipe['data'], id=recipe['id'])
+    try:
+        response = get_api_response("/random")
+        recipe = response['recipe']
+        return render_template('base_recipe.html', data=recipe['data'], id=recipe['id'])
+    except Exception as e:
+        return render_template('error.html', message=str(e))
 
 
-@app.route('/cuisines/search', methods=['GET'])
+@app.route('/search/cuisines', methods=['GET'])
 def search_recipes_by_cuisine():
     cuisine = request.args.get('cuisine')
-    response = get_api_response(f"/cuisines/{cuisine}")
-    recipes = response['cuisines']['data']
-    return render_template('search.html', data=recipes)
+    try:
+        response = get_api_response(f"/cuisines/{cuisine}")
+        recipes = response['cuisines']['data']
+        return render_template('search.html', data=recipes)
+    except Exception as e:
+        return render_template('error.html', message=str(e))
 
 
-@app.route('/categories/search', methods=['GET'])
+@app.route('/search/categories', methods=['GET'])
 def get_recipes_by_category():
     category = request.args.get('category')
-    response = get_api_response(f"/categories/{category}")
-    recipes = response['categories']['data']
-    return render_template('search.html', data=recipes)
+    try:
+        response = get_api_response(f"/categories/{category}")
+        recipes = response['categories']['data']
+        return render_template('search.html', data=recipes)
+    except Exception as e:
+        return render_template('error.html', message=str(e))
 
 
 @app.route('/recipe', methods=['GET'])
 def get_recipe_details():
     recipe_id = request.args.get('recipe_id')
-    response = get_api_response(f"/details/{recipe_id}")
-    recipe = response['recipe']
-    return render_template('base_recipe.html', data=recipe['data'], id=recipe['id'])
+    try:
+        response = get_api_response(f"/details/{recipe_id}")
+        recipe = response['recipe']
+        return render_template('base_recipe.html', data=recipe['data'], id=recipe['id'])
+    except Exception as e:
+        return render_template('error.html', message=str(e))
 
 
 @app.route('/recommendations', methods=['GET'])
 def get_recommendations():
     recipe_id = request.args.get('recipe_id')
-    response = get_api_response(f"/recommendations/{recipe_id}")
-    recommendations = response['recommendations']
-    return render_template('recommendations.html', data=recommendations['data'],
-                           targetID=recommendations['targetID'])
+    try:
+        response = get_api_response(f"/recommendations/{recipe_id}")
+        recommendations = response['recommendations']
+        return render_template('recommendations.html', data=recommendations['data'], targetID=recommendations['targetID'])
+    except Exception as e:
+        return render_template('error.html', message=str(e))
 
 
-@app.route('/search', methods=['GET'])
+@app.route('/phrase', methods=['GET'])
 def search_recipes():
     phrase = request.args.get('phrase')
-    response = get_api_response(f"/search/{phrase}")
-    recipes = response['recipes']
-    return render_template('search.html', data=recipes['data'])
+    try:
+        response = get_api_response(f"/search/{phrase}")
+        recipes = response['recipes']
+        return render_template('search.html', data=recipes['data'])
+    except Exception as e:
+        return render_template('error.html', message=str(e))
 
 
 @app.route('/search_ingredients', methods=['POST'])
 def search_by_ingredients():
     ingredients = request.form['ingredients']
     payload = json.dumps({"ingredients": ingredients})
-    response = post_api_response("/search", payload)
-    recipes = response['recipe']['data']
-    return render_template('search.html', data=recipes)
+    try:
+        response = post_api_response("/search", payload)
+        recipes = response['recipe']['data']
+        return render_template('search.html', data=recipes)
+    except Exception as e:
+        return render_template('error.html', message=str(e))
 
 
 @app.route('/generate_meal_plan', methods=['POST'])
 def generate_meal_plan():
     preference = request.form['preference']
     payload = json.dumps({"preference": preference})
-    response = post_api_response("/generate/meal_plan", payload)
-    meal_plan = response['recipe']['data']
-    return render_template('meal_plan.html', data=meal_plan)
+    try:
+        response = post_api_response("/generate/meal_plan", payload)
+        meal_plan = response['recipe']['data']
+        return render_template('meal_plan.html', data=meal_plan)
+    except Exception as e:
+        return render_template('error.html', message=str(e))
 
 
 @app.route('/create_shopping_list', methods=['GET', 'POST'])
@@ -122,17 +156,24 @@ def create_shopping_list():
     if request.method == 'POST':
         recipe_id = request.form['recipe_id']
         payload = json.dumps({"id": recipe_id})
-        response = post_api_response("/generate/shopping_list", payload)
-        shopping_list = response['lists']['data']
-        return render_template('shopping_list.html', data=shopping_list)
+        try:
+            response = post_api_response("/generate/shopping_list", payload)
+            shopping_list = response['lists']['data']
+            return render_template('shopping_list.html', data=shopping_list)
+        except Exception as e:
+            return render_template('error.html', message=str(e))
     return render_template('shopping_list.html')
 
 
+#try jest tylko w tej metodzie, bo send_whatsapp_form i send_whatsapp mogą być otworzone tylko za pomocą tej metody
 @app.route('/whatsapp_instructions', methods=['GET'])
 def whatsapp_instructions():
     message_type = request.args.get('message_type')
     message_content = request.args.get('message_content')
-    return render_template('whatsapp_instructions.html', message_type=message_type, message_content=message_content)
+    try:
+        return render_template('whatsapp_instructions.html', message_type=message_type, message_content=message_content)
+    except Exception as e:
+        return render_template('error.html', message=str(e))
 
 
 @app.route('/send_whatsapp_form', methods=['GET'])
